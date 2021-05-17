d77fbd2c445cca5a290e329f61de9b2c66d6d6bd
yoo-kie <alice0926@hanmail.net>
2021-05-17 23:53:22 +0900

    ✏️ 딕셔너리 default 공부

--- TIL/Quiz.playground/Contents.swift
+++ TIL/Quiz.playground/Contents.swift
@@ -15,3 +15,16 @@ plus.x = 5
 
 // what's the final value of plus.x?
 // struct는 프로퍼티로 재귀적인 retain cycle을 형성할 수 없음-!
+
+var messages = [200:"OK", 404:"Not Found"]
+var foo: (Int) -> String = { code in
+    if code > 500 {
+        return "Server"
+    }
+    return "Unknown"
+}
+
+let message = messages[300, default:foo(300)]
+
+// what's the value of message?
+// default에 대해 알아볼 것-!
