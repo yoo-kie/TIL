dbc20206d538823915269e126ed921172d42f166
yoo-kie <alice0926@hanmail.net>
2021-05-22 23:54:33 +0900

    ✏️ unsafe 예제

--- TIL/TIL/ViewController.swift
+++ TIL/TIL/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        var arr1: [Int] = [1, 2, 3, 4, 5]
+        var arr2 = arr1
+        // 1
+        withUnsafePointer(to: &arr1) { address in
+            print(address)
+        }
+        // 2
+        withUnsafePointer(to: &arr2) { address in
+            print(address)
+        }
+
+        arr2.append(2)
+        // 3
+        withUnsafePointer(to: &arr2) { address in
+            print(address)
+        }
     }
 
-
 }
-
